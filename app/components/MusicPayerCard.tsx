eeed9e92cc348640fd11c57b30380ee41a7e8fa8
HamzaK7329 <hamza.k7329@gmail.com>
2026-05-11 18:59:19 +0400

    Added songs/cover images + playing functionality

--- app/components/MusicPayerCard.tsx
+++ app/components/MusicPayerCard.tsx
@@ -2,7 +2,93 @@
 
 import { useRef, useState } from "react";
 
+type Song = {
+    title: string
+    artist: string
+    cover: string
+    audio: string
+}
+
+const songs: Song[] = [
+    {
+        title: "Which One",
+        artist: "Drake",
+        cover: "/covers/DrakeWhichOneCover.png",
+        audio:"/songs/DrakeWhichOneSong.mp3"
+    }
+]
+
 export default function MusicPlayerCard() {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    const [currentSong, setCurrentSong] = useState<number>(0);
+    const [isPlaying, setIsPLaying] = useState<boolean>(false);
+    const [progress, setProgress] = useState<number>(0);
+    const [duration, setDuration] = useState<number>(0);
+
+    const song = songs[currentSong];
+    
+    const togglePlay = async () => {
+        if (!audioRef.current) return;
+
+        if (isPlaying) {
+            audioRef.current.pause();
+            setIsPLaying(false);
+        } else {
+            await audioRef.current.play();
+            setIsPLaying(true);
+        }
+    }
+
+    const nextSong = () => {
+        const next = (currentSong + 1) % songs.length
+        setCurrentSong(next)
+        setProgress(0)
+
+        if (isPlaying) {
+            setTimeout(() => {
+                audioRef.current?.play()
+            }, 0)
+        }
+    }
+
+    const prevSong = () => {
+    const prev = (currentSong - 1) % songs.length
+    setCurrentSong(prev)
+    setProgress(0)
+
+    if (isPlaying) {
+        setTimeout(() => {
+            audioRef.current?.play()
+        }, 0)
+        }
+    }
+
+    const updateProgress = () => {
+        if (!audioRef.current) return;
+
+        setProgress(audioRef.current.currentTime);
+        setDuration(audioRef.current.duration || 0);
+    }
+
+    const handleProgressChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!audioRef.current) return
+
+        const value = Number(e.target.value)
+        audioRef.current.currentTime = value
+        setProgress(value);
+    }
+
+    const formatTime = (time: number) => {
+        if (!time) return "0:00"
+
+        const minutes = Math.floor(time / 60)
+        const seconds = Math.floor(time % 60)
+
+        return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+    }
+
+
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="w-72.5 rounded-[22px] overflow-hidden bg-[#ffffff] shadow-2xl " >
@@ -10,29 +96,29 @@ export default function MusicPlayerCard() {
         {/* Music Cover */}
         <div className="relative h-57.5 " >
             <img
-                src={"public/globe.svg"}
-                alt={"Dummy ALT"}
+                src={song.cover}
+                alt={song.title}
                 className="w-full h-full object-cover"
             />
 
             <button className="absolute top-3 right-3 w-7 h-7 rounded-full bg-white text-black flex items-center justify-center text-sm shadow " >
-                :
+                ⁝
             </button>
         </div>
 
         {/* Music Info */}
         <div className="px-5 pt-4 flex items-center justify-between " >
-            <span className="text-sm text-black" >Back</span>
+            <span className="text-sm text-black" >⬅︎</span>
 
             <div className="text-center">
                 <h2 className="text-[16px] font-semibold text-black ">
-                    Music Title
+                    {song.title}
                 </h2>
                 <p className="text-[11px] text-black/60 mt-1 " >
-                    Music Artist
+                    {song.artist}
                 </p>
             </div>
-            <span className="text-sm text-black " >Love</span>
+            <span className="text-sm text-black " >❤︎</span>
         </div>
 
         {/* Progress */}
@@ -40,13 +126,15 @@ export default function MusicPlayerCard() {
             <input
                 type="range"
                 min={0}
-                max={100}
+                max={duration || 0}
+                value={progress}
+                onChange={handleProgressChange}
                 className="w-full accent-black cursor-pointer "
             />
 
             <div className="flex justify-between text-[10px] text-black/60 mt-1 " >
-                <span>Progress</span>
-                <span>Duration</span>
+                <span>{formatTime(progress)}</span>
+                <span>{formatTime(duration)}</span>
             </div>
         </div>
 
@@ -54,23 +142,34 @@ export default function MusicPlayerCard() {
         <div className="flex items-center justify-center gap-6 py-6 " >
             <button
                 className="text-lg cursor-pointer"
+                onClick={prevSong}
             >
-                Back
+                ⏮︎
             </button>
 
             <button 
             className="w-11 h-11 rounded-full bg-black text-white flex items-center justify-center shadow-lg cursor-pointer " 
-            
-            > Play/Pause
+            onClick={togglePlay}
+            > ⏯︎
             </button>
 
             <button
                 className="text-lg cursor-pointer"
+                onClick={nextSong}
             >
+                ⏭︎
             </button>
 
         </div>
 
+        <audio
+            ref={audioRef}
+            src={song.audio}
+            onTimeUpdate={updateProgress}
+            onLoadedMetadata={updateProgress}
+            onEnded={nextSong}
+        
+        />
       </div>
     </div>
   );
